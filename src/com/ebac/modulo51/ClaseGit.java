c054d3251df34d5909d7b5f8f44a83dbf1af4916
AngelBrasil <angel_brasil18@hotmail.com>
2026-01-14 14:02:50 -0600

    Agrega otra linea de impresion en consola

--- src/com/ebac/modulo51/ClaseGit.java
+++ src/com/ebac/modulo51/ClaseGit.java
@@ -4,5 +4,6 @@ public class ClaseGit {
     public static void main(String[] args) {
         System.out.println("Clase Java para uso de repositorio git");
         System.out.println("Actualización... nueva rama en git");
+        System.out.println("Nueva rama... en git");
     }
 }
